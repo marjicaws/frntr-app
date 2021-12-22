d85ce602eebf2d9631b1374d707eede50857151a
eterral <elliott@eterral.com>
2021-12-22 15:49:56 -0600

    added restrict helped to produt routes

--- routes/products.js
+++ routes/products.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import * as controllers from "../controllers/products.js";
+import restrict from "../helpers/restrict.js";
 
 const router = Router();
 router.get("/products", controllers.getProducts);
 router.get("/products/furniture", controllers.getFurniture);
 router.get("/products/kitchen", controllers.getKitchen);
 router.get("/products/backyard", controllers.getBackyard);
-router.post("/products", controllers.createProduct);
-router.put("/products/:id", controllers.updateProduct);
-router.delete("/products/:id", controllers.deleteProduct);
+router.post("/products", restrict, controllers.createProduct);
+router.put("/products/:id", restrict, controllers.updateProduct);
+router.delete("/products/:id", restrict, controllers.deleteProduct);
 router.get("/products/:id", controllers.getProduct);
 export default router;
