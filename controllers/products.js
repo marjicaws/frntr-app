57483dd1f6e45af72f2f0713dc9440980b3c9c2f
Michael Tan <michaeltan@MacBook-Pro.fios-router.home>
2021-12-22 11:58:52 -0500

    added the delete

--- controllers/products.js
+++ controllers/products.js
@@ -50,3 +50,24 @@ export const createProduct = async (req, res) => {
     res.status(500).json({ error: error.message })
   }
 }
+
+export const updateProduct = async (req, res) => {
+  const { id } = req.params
+  const product = await Product.findByIdAndUpdate(id, req.body, { new: true })
+  res.status(200).json(post)
+}
+
+export const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params
+    const deleted = await Product.findByIdAndDelete(id)
+    if (deleted) {
+      return res.status(200).send('Post was deleted')
+    }
+    throw new Error('Posts not found')
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).json({ error: error.message })
+  }
+}
+
--- routes/products.js
+++ routes/products.js
@@ -7,5 +7,6 @@ router.get('/products/furniture', controllers.getFurniture)
 router.get('/products/kitchen', controllers.getKitchen)
 router.get('/products/backyard', controllers.backyard)
 router.get('/products', controllers.createProduct)
-
+router.get('/products/:id', contollers.updateProduct)
+router.get('/products/:id', controllers.deleteProduct)
 export default router
