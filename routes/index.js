8f828612090754a5886c38e6d1785da23e179a93
eterral <elliott@eterral.com>
2021-12-22 15:19:07 -0600

    cleaned up exports in routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import productsRoutes from "./products";
-import usersRoutes from "./users";
+import productsRoutes from "./products.js";
+import usersRoutes from "./users.js";
 
-export const router = Router();
+const router = Router();
 
 router.get("/", (req, res) => res.send("This is furniture API"));
 router.use("/", productsRoutes);
 router.use("/", usersRoutes);
+
+export default router;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import * as controllers from "../controllers/users"
+import * as controllers from "../controllers/users.js";
 
+const router = Router();
 
+router.post("/account/signup", controllers.signUp);
+router.post("/account/signIn", controllers.signIn);
+router.get("/account/verify", controllers.verify);
 
-export const router = Router()
-
-router.post('/account/signUp', controllers.signUp)
-router.post('/account/signIn', controllers.signIn)
-router.get('/account/verify', controllers.verify)
+export default router;
