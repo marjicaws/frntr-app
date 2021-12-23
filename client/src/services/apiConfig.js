bb9571d6d9f9e7c02e72e11e81f607aae52a2f0f
eterral <elliott@eterral.com>
2021-12-23 15:21:27 -0600

    services/users and apiConfig

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { app } from "faker/lib/locales/en";
 
 const api = axios.create({
   baseURL:
@@ -6,5 +7,19 @@ const api = axios.create({
       ? "https://floating-atoll-11194.herokuapp.com/api"
       : "http://localhost:3000/api/",
 });
+
+const getToken = () => {
+  return new Promise((resolve) => {
+    resolve(`Bearer ${localStorage.getItem("token") || null}`);
+  });
+};
+
+app.inteceptors.request.use(async function (config) {
+  config.headers(`Authorization`) = await getToken()
+  return config
+}, function (error) {
+  console.log('Request error: ', error)
+  return Promise.reject(error)
+})
 
 export default api;
