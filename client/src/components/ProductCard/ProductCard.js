51312b36f226aef454aa9fd89438090b0fa82bb8
eterral <elliott@eterral.com>
2021-12-23 15:16:04 -0600

    fixed card component

--- client/src/components/ProductCard/ProductCard.js
+++ client/src/components/ProductCard/ProductCard.js
@@ -5,8 +5,8 @@ import "./ProductCard.css";
 export default function ProductCard(props) {
   return (
     <div key={props.key}>
-      <Link to={`/products/${props._id}`}>
-        <img src={props.img} alt={props._id} />
+      <Link to={`/products/${props.id}`}>
+        <img src={props.img} alt={props.id} />
       </Link>
       <h4>{props.title}</h4>
       <h6>{props.price}</h6>
