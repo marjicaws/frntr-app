29b3740d5cba2b96bead8127c9ef249786e23717
jerrytek <jerrycodingtek@gmail.com>
2022-01-03 13:41:23 -0600

    Added Carousel component with css

--- client/src/components/Carosel/Carosel.js
+++ client/src/components/Carosel/Carosel.js
@@ -1,13 +1,15 @@
 import Carousel from 'react-bootstrap/Carousel'
+import './Carosel.css'
 
 export default function Carosel() {
   return (
-    <div>
-<Carousel>
-  <Carousel.Item>
+    <div className='more-containers'>
+    <div className='carousel-container'>
+      <Carousel>
+  <Carousel.Item interval={6000}>
     <img
       className="d-block w-100"
-      src="holder.js/800x400?text=First slide&bg=373940"
+      src="https://i.imgur.com/DQSeVqI.gif"
       alt="First slide"
     />
     <Carousel.Caption>
@@ -15,13 +17,12 @@ export default function Carosel() {
       <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
     </Carousel.Caption>
   </Carousel.Item>
-  <Carousel.Item>
+  <Carousel.Item interval={2500}>
     <img
       className="d-block w-100"
-      src="holder.js/800x400?text=Second slide&bg=282c34"
+      src="https://i.imgur.com/DQSeVqI.gif"
       alt="Second slide"
     />
-
     <Carousel.Caption>
       <h3>Second slide label</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -30,16 +31,16 @@ export default function Carosel() {
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src="holder.js/800x400?text=Third slide&bg=20232a"
+      src="https://i.imgur.com/DQSeVqI.gif"
       alt="Third slide"
     />
-
     <Carousel.Caption>
       <h3>Third slide label</h3>
       <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
     </Carousel.Caption>
   </Carousel.Item>
 </Carousel>
+      </div>
     </div>
   )
 }
