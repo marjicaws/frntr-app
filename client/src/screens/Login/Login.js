b3d37fda44c688dabbaab2ee32f37482a16186b2
Michael Tan <michaeltan@MacBook-Pro.fios-router.home>
2022-01-03 15:16:13 -0500

    added the css and toggle for the login

--- client/src/screens/Login/Login.js
+++ client/src/screens/Login/Login.js
@@ -1,17 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import SignIn from '../../components/SignUp/SignUp.js'
+import React, { useState } from 'react'
+import './Login.css'
+import Layout from "../../components/Layout/Layout";
+import SignIn from '../../components/SignIn/SignIn.js'
 import SignUp from '../../components/SignUp/SignUp.js'
 
 
-
-
 function Login() {
+
+  const [login, setLogin] = useState(true)
+
+  const handleSignIn = () => {
+    setLogin(true)
+  }
+
+  const handleCreate = () => {
+    setLogin(false)
+  }
+
   return (
+    <Layout>
     <div>
-      <Link to='signin'>Sign In</Link>
-      <Link to='signup'>Create Account</Link>
+        <button className="signin_toggle"
+          onClick={handleSignIn}>Sign In</button>
+        <button className="signup_toggle"
+          onClick={handleCreate}>Create Account</button>
+        {
+          login ? <SignIn /> : <SignUp />
+        }
     </div>
+    </Layout>
   )
 }
 
