d436e549e5ac710cd498ee782774ba9bfa65f1e1
jerrytek <jerrycodingtek@gmail.com>
2022-01-04 09:23:13 -0600

    about us page css done

--- client/src/screens/AboutUs/AboutUs.js
+++ client/src/screens/AboutUs/AboutUs.js
@@ -4,23 +4,25 @@ import Layout from "../../components/Layout/Layout";
 export default function About() {
   return (
     <Layout>
-      <div className="about-container">
-        <div>
-            <h1>About Us</h1>
-          <div>
+      <div className='big-container-about'>
+        <div className='join-about'>
+        <div className="about-container">
+          <h1>About Us</h1>
+          </div>
             {" "}
-            <h4>
+            <h4 className='about-text'>
               All of our products are made carefully and use only premium
               materials.
             </h4>
+            </div>
             <div className="github">
             <div className="profile"><p>Marjica Watson-Santos</p><a><img src="https://i.imgur.com/PfMoKGt.png"/></a><a><img src="https://i.imgur.com/COYA784.png"/></a></div>
             <div className="profile"><p>Michael Tan</p><a><img src="https://i.imgur.com/PfMoKGt.png"/></a><a><img src="https://i.imgur.com/COYA784.png"/></a></div>
             <div className="profile"><p>Elliott Terral</p><a><img src="https://i.imgur.com/PfMoKGt.png"/></a><a><img src="https://i.imgur.com/COYA784.png"/></a></div>
             <div className="profile"><p>Jerry Gonzalez</p><a><img src="https://i.imgur.com/PfMoKGt.png"/></a><a><img src="https://i.imgur.com/COYA784.png"/></a></div>
             </div>
-          </div>
-        </div>
+          
+        
       </div>
     </Layout>
   );
